f98a0155b38fb3a7777537f9579ab3325f7da934
shivamjadhav <jadhavshivam0228@gmail.com>
2024-11-02 20:26:32 -0500

    added lsa text summarization a nd bert summarization

--- topicm.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-df=pd.read_csv('./dataset/quora_questions.csv')
-data=list(df['Question'])
-cv=CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
-dtm=cv.fit_transform(data)
-print(dtm)
